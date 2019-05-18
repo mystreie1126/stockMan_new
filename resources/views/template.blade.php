443900547f9e1e6c8b900539648c5694944f8884
Jian <jianqilu1126@gmail.com>
2019-05-18 02:32:16 +0100

    save

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -38,7 +38,7 @@
       //const stockMan = 'http://localhost/project/laravel/newStockApi/public/api/';
       //const api = 'http://localhost/project/laravel/stockManager_api/public/api';
       //var stockMan = 'http://stockangryman.funtech.ie/api/';
-      var stockMan = 'http://stockmangagerapi.funtech.ie/api/';
+      var stockMan = 'https://calm-anchorage-96610.herokuapp.com/http://stockmangagerapi.funtech.ie/api/';
       //var stockMan = 'http://localhost/project/laravel/stockManager_api/public/api/';
     </script>
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
